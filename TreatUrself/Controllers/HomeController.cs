e77aaff23b97bebb474b5af8285471f717ebb3ce
Patrick Lee <patricklee1138@gmail.com>
2021-06-14 20:46:05 -0700

    add handling to display all Treats & Flavors on splash

--- TreatUrself/Controllers/HomeController.cs
+++ TreatUrself/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using TreatUrself.Models;
 
 namespace TreatUrself.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly TreatUrselfContext _db;
+
+    public HomeController(TreatUrselfContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      List<Flavor> allFlavors = _db.Flavors.ToList();
+      model.Add("Flavors", allFlavors);
+      List<Treat> allTreats = _db.Treats.ToList();
+      model.Add("Treats", allTreats);
+      return View(model);
     }
   }
 }
