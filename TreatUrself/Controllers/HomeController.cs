399c7225110bb3e6b6696935f9b66dd87c7609b5
Patrick Lee <patricklee1138@gmail.com>
2021-06-14 20:56:01 -0700

    remove unnecessary 'using' directives

--- TreatUrself/Controllers/HomeController.cs
+++ TreatUrself/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using TreatUrself.Models;
